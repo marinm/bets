61fd35253ed0c935f59222cbda5752d55f8c0ab6
Marin <marinmema@live.ca>
2026-06-09 21:14:34 -0400

    add hasMany relationships

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -7,4 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class Team extends Model
 {
     protected $fillable = ['country_code', 'long_name'];
+
+    public function fixturesAsTeam1()
+    {
+        return $this->hasMany(Fixture::class, 'team_1_id');
+    }
+
+    public function fixturesAsTeam2()
+    {
+        return $this->hasMany(Fixture::class, 'team_2_id');
+    }
+
+    public function winningFixtures()
+    {
+        return $this->hasMany(Fixture::class, 'winning_team_id');
+    }
+
+    public function bets()
+    {
+        return $this->hasMany(Bet::class, 'winner_team_id');
+    }
 }
