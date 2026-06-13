9b5b14fa2fe16318796de6bf2bf036fabd19c418
Marin <marinmema@live.ca>
2026-06-13 10:47:11 -0400

    pass in user

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -8,15 +8,20 @@ class FeedController extends Controller
 {
     public function __invoke()
     {
+        $user = request()->user();
+
         $fixtures = Fixture::withCount('bets')
-            ->with(['team1', 'team2', 'bets' => function ($query) {
-                $query->where('user_id', request()->user()->id);
+            ->with(['team1', 'team2', 'bets' => function ($query) use ($user) {
+                $query->where('user_id', $user->id);
             }])
             ->orderBy('started_at')
             ->get();
 
+        $user->loadCount('bets');
+
         return view('feed', [
             'fixtures' => $fixtures,
+            'user' => $user,
         ]);
     }
 }
--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -3,15 +3,16 @@
 @section('title', config('app.name'))
 
 @section('content')
-    @auth
-        <div class="w-full py-4 flex justify-between items-center mb-4">
-            <span class="text-white mr-4">{{ Auth::user()->name }}</span>
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <button type="submit" class="text-gray-500">Logout</button>
-            </form>
-        </div>
-    @endauth
+    <div class="w-full py-4 flex justify-between items-center mb-4">
+        <span class="text-white mr-4">{{ $user->name }}</span>
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button type="submit" class="text-gray-500">Logout</button>
+        </form>
+    </div>
+    <div class="text-white mb-4">
+        {{ $user->bets_count }} {{ Str::plural('bet', $user->bets_count) }} placed
+    </div>
     <div class="flex flex-col gap-4">
         @foreach ($fixtures as $fixture)
             <a href="{{ route('fixture-bets.create', ['fixture' => $fixture]) }}" class="bg-gray-800 p-4 rounded-xl flex justify-between items-center">
