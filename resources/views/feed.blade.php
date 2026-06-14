078479c739a2d6d06f5a0f002e74ab083ef85f39
Marin <marinmema@live.ca>
2026-06-14 13:20:43 -0400

    compact index card

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -15,17 +15,22 @@
     </div>
     <div class="flex flex-col gap-4">
         @foreach ($fixtures as $fixture)
-            <a href="{{ route('fixture-bets.create', ['fixture' => $fixture]) }}" class="bg-gray-800 p-4 rounded-xl flex justify-between items-center">
-                <div class="flex-1">
-                    <p class="text-white text-lg font-bold truncate">{{ $fixture->team1->long_name }}</p>
-                    <p class="text-white text-lg font-bold truncate">{{ $fixture->team2->long_name }}</p>
-                    <p class="text-gray-400">{{ $fixture->started_at->format('Y-m-d H:i') }} - {{ $fixture->is_finished ? 'Finished' : 'Upcoming' }}</p>
+            <a href="{{ route('fixture-bets.create', ['fixture' => $fixture]) }}" class="bg-gray-800 p-4 rounded-xl">
+                <p class="text-white text-lg font-bold truncate">
+                    <span class="fi fi-{{ strtolower($fixture->team1->country_code) }}"></span>
+                    {{ $fixture->team1->long_name }}
+                </p>
+                <p class="text-white text-lg font-bold truncate">
+                    <span class="fi fi-{{ $fixture->team2->country_code }}"></span>
+                    {{ $fixture->team2->long_name }}
+                </p>
+                <div class="flex justify-between items-center mt-2">
+                    <p class="text-gray-400">{{ $fixture->started_at->format('D, M d g:i A') }}</p>
+                    @if ($fixture->bets->isNotEmpty())
+                        <p class="text-green-400">Bet Placed</p>
+                    @endif
                     <p class="text-gray-400">{{ $fixture->bets_count }} {{ Str::plural('bet', $fixture->bets_count) }}</p>
                 </div>
-                @if ($fixture->bets->isNotEmpty())
-                    <p class="text-green-400">Bet Placed</p>
-                @endif
-                
             </a>
         @endforeach
     </div>
