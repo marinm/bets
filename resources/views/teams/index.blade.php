6cd941f74a65f1cef820816ae109edcdaa099a3f
Marin <marinmema@live.ca>
2026-06-15 23:46:01 -0400

    compact list

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -8,11 +8,14 @@
         <a href="{{ route('teams.create') }}" class="px-4 py-2 bg-blue-500 text-white rounded">Create</a>
     </div>
 
-    <div class="flex flex-col gap-4">
+    <div class="flex flex-col border border-white rounded text-white">
         @foreach ($teams as $team)
-            <a href="{{ route('teams.show', ['team' => $team]) }}" class="bg-gray-800 p-4 rounded flex justify-between items-center">
-                <h2 class="text-white text-lg font-bold">{{ $team->long_name }}</h2>
-                <p class="text-gray-400">{{ $team->country_code }}</p>
+            <a href="{{ route('teams.show', ['team' => $team]) }}" class="border-b border-white p-4 flex justify-between items-center">
+                <h2 class="">{{ $team->long_name }}</h2>
+                <div class="flex items-center gap-2">
+                    <p class="font-mono">{{ $team->country_code }}</p>
+                    <span class="fi fi-{{ strtolower($team->country_code) }} text-4xl"></span>
+                </div>
             </a>
         @endforeach
     </div>
