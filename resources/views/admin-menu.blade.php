1dd1ec2000c84504fbca97d9fca42430e487de93
Marin <marinmema@live.ca>
2026-06-15 23:39:35 -0400

    restyle menu

--- resources/views/admin-menu.blade.php
+++ resources/views/admin-menu.blade.php
@@ -3,11 +3,12 @@
 @section('title', config('app.name'))
 
 @section('content')
-    <div class="flex flex-col justify-start items-start gap-1">
-        <a href="{{ route('users.index') }}" class="w-full text-white mb-4 inline-block border border-whites p-4 rounded">Users</a>
-        <a href="{{ route('teams.index') }}" class="w-full text-white mb-4 inline-block border border-whites p-4 rounded">Teams</a>
-        <a href="{{ route('fixtures.index') }}" class="w-full text-white mb-4 inline-block border border-whites p-4 rounded">Fixtures</a>
-        <a href="{{ route('bets.index') }}" class="w-full text-white mb-4 inline-block border border-whites p-4 rounded">Bets</a>
-        <a href="{{ route('one-time-tokens.index') }}" class="w-full text-white mb-4 inline-block border border-whites p-4 rounded">One-Time Tokens</a>
+    <h1 class="text-white font-bold my-4">Admin menu</h1>
+    <div class="border border-white rounded flex flex-col justify-start items-start">
+        <a href="{{ route('bets.index') }}" class="w-full text-white inline-block border-b border-white p-4">Bets</a>
+        <a href="{{ route('fixtures.index') }}" class="w-full text-white inline-block border-b border-white p-4">Fixtures</a>
+        <a href="{{ route('one-time-tokens.index') }}" class="w-full text-white inline-block border-b p-4">One-Time Tokens</a>
+        <a href="{{ route('teams.index') }}" class="w-full text-white inline-block border-b border-white p-4">Teams</a>
+        <a href="{{ route('users.index') }}" class="w-full text-white inline-block border-white p-4">Users</a>
     </div>
 @endsection
