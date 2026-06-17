6e3aebdd5b859810eb49d32d3af510840969aaa2
Marin <marinmema@live.ca>
2026-06-17 18:13:47 -0400

    prettier qr code page

--- resources/views/one-time-tokens/show.blade.php
+++ resources/views/one-time-tokens/show.blade.php
@@ -3,24 +3,24 @@
 @section('title', 'One-Time Token')
 
 @section('content')
-    <a href="{{ route('one-time-tokens.index') }}" class="text-gray-400 hover:text-white mb-4 inline-block">One-Time Tokens</a>
+    <a href="{{ route('one-time-tokens.index') }}" class="text-gray-400 hover:text-white mb-4 inline-block">Back</a>
     <h1 class="text-white text-2xl font-bold mb-6">One-Time Token</h1>
 
-    <div class="bg-gray-800 p-4 rounded">
-        <p class="text-gray-400">User: {{ $oneTimeToken->user->name }}</p>
-        <p class="text-gray-400">Secret: {{ $oneTimeToken->secret }}</p>
-        <p class="text-gray-400">
-            Sign-in Link:
-            <a href="{{ $url }}" class="text-blue-500 hover:text-blue-400">
-                {{ $url }}
-            </a>
-        </p>
-        <p class="text-gray-400">Created: {{ $oneTimeToken->created_at->format('Y-m-d H:i') }}</p>
+    <p class="text-gray-600 text-center">
+        {{ $oneTimeToken->created_at->format('Y-m-d h:i A') }}
+    </p>
 
-        <div id="qrcode" class="flex justify-center mt-4"></div>
+    <p class="text-gray-600 text-center">
+        {{ $oneTimeToken->created_at->diffForHumans() }}
+    </p>
+
+    <div class="bg-white rounded p-2 m-auto w-fit mt-6">
+        <div id="qrcode"></div>
     </div>
 
-    <form action="{{ route('one-time-tokens.destroy', $oneTimeToken) }}" method="POST" class="mt-4">
+    <p class="text-white text-center text-lg bold">{{ $oneTimeToken->user->name }}</p>
+
+    <form action="{{ route('one-time-tokens.destroy', $oneTimeToken) }}" method="POST" class="mt-12">
         @method('DELETE')
         @csrf
         <button type="submit" class="w-full px-4 py-2 bg-red-500 text-white rounded">Delete</button>
@@ -32,8 +32,8 @@
     <script>
         new QRCode(document.getElementById("qrcode"), {
             text: "{{ $url }}",
-            width: 200,
-            height: 200,
+            width: 300,
+            height: 300,
             colorDark : "#ffffff",
             colorLight : "#000000",
         });
