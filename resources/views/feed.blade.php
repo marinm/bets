da2a72fee7f6d7d572a2115a49c1f27197912a1e
Marin <marinmema@live.ca>
2026-06-16 21:07:32 -0400

    more compact feed

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -3,8 +3,7 @@
 @section('title', config('app.name'))
 
 @section('content')
-    <div class="w-full py-4 flex justify-between items-center mb-4 text-white">
-        <span>{{ $user->bets_count }} {{ Str::plural('bet', $user->bets_count) }} placed</span>
+    <div class="w-full py-4 flex justify-end items-center mb-4 text-white">
         <a href="{{ route('profile.show') }}" class="flex items-center gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
@@ -14,22 +13,14 @@
     </div>
     <div class="flex flex-col gap-4">
         @foreach ($fixtures as $fixture)
-            <a href="{{ route('fixture-bets.create', ['fixture' => $fixture]) }}" class="bg-gray-800 p-4 rounded-xl">
-                <p class="text-white text-lg font-bold truncate">
+            <a href="{{ route('fixture-bets.create', ['fixture' => $fixture]) }}" class="bg-gray-900 p-4 rounded-xl flex justify-between items-center gap-4">
+                <p class="text-gray-400 font-mono">{{ $fixture->started_at->format('M d') }}</p>
+                <div class="text-2xl font-mono text-white">
                     <span class="fi fi-{{ strtolower($fixture->team1->country_code) }}"></span>
-                    {{ $fixture->team1->long_name }}
-                </p>
-                <p class="text-white text-lg font-bold truncate">
+                    -
                     <span class="fi fi-{{ strtolower($fixture->team2->country_code) }}"></span>
-                    {{ $fixture->team2->long_name }}
-                </p>
-                <div class="flex justify-between items-center mt-2">
-                    <p class="text-gray-400">{{ $fixture->started_at->format('D, M d g:i A') }}</p>
-                    @if ($fixture->bets->isNotEmpty())
-                        <p class="text-green-400">Bet Placed</p>
-                    @endif
-                    <p class="text-gray-400">{{ $fixture->bets_count }} {{ Str::plural('bet', $fixture->bets_count) }}</p>
                 </div>
+                <p class="text-gray-400 font-mono">{{ $fixture->bets_count }}</p>
             </a>
         @endforeach
     </div>
