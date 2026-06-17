5909d6712245af6b5165e5629b7146a609bf1e90
Marin <marinmema@live.ca>
2026-06-16 23:28:11 -0400

    prettier betting page

--- resources/views/fixture-bets/create.blade.php
+++ resources/views/fixture-bets/create.blade.php
@@ -5,42 +5,53 @@
 @section('content')
     <a href="{{ route('feed') }}" class="block mt-2 text-gray-400">Back</a>
 
-    <div class="bg-gray-800 p-4 mt-10 mb-4 rounded flex justify-between items-center">
-        <div class="flex-1">
+    <div class="bg-gray-800 p-4 mt-10 mb-4 rounded border border-white">
+        <div class="flex items-center justify-start gap-1">
+            <span class="fi fi-{{ strtolower($fixture->team1->country_code) }}"></span>
             <p class="text-white text-lg font-bold truncate">{{ $fixture->team1->long_name }}</p>
-            <p class="text-white text-lg font-bold truncate">{{ $fixture->team2->long_name }}</p>
-            <p class="text-gray-400">{{ $fixture->started_at->format('Y-m-d H:i') }}</p>
         </div>
-        <p class="text-gray-400">{{ $fixture->is_finished ? 'Finished' : 'Upcoming' }}</p>
+        <div class="flex items-center justify-start gap-1">
+            <span class="fi fi-{{ strtolower($fixture->team2->country_code) }}"></span>
+            <p class="text-white text-lg font-bold truncate">{{ $fixture->team2->long_name }}</p>
+        </div>
+        <p class="text-gray-400 mt-4">{{ $fixture->started_at->format('D, M j h:i A') }}</p>
     </div>
 
     <form action="{{ route('fixture-bets.store', ['fixture' => $fixture]) }}" method="POST">
         @method('POST')
         @csrf
-        <fieldset class="border border-white p-4 rounded mb-4">
-            <legend class="text-white px-2">Winner</legend>
-            
-            <label class="flex items-center text-white mb-3">
-                <input type="radio" name="winner_team_id" value="{{ $fixture->team1->id }}" {{ old('winner_team_id') == $fixture->team1->id ? 'checked' : '' }} class="mr-2" />
-                {{ $fixture->team1->long_name }}
+        <div class="border border-white rounded">
+            <label class="flex justify-between items-center text-white p-4 border-b border-white">
+                <div>
+                    <input type="radio" name="winner_team_id" value="{{ $fixture->team1->id }}" {{ old('winner_team_id') == $fixture->team1->id ? 'checked' : '' }} class="mr-2" />
+                    {{ $fixture->team1->long_name }}
+                </div>
+                <div class="font-mono text-white-800">
+                    {{ $fixture->team1->country_code }}
+                    <span class="fi fi-{{ strtolower($fixture->team1->country_code) }}"></span>
+                </div>
             </label>
-
-            <label class="flex items-center text-white mb-3">
-                <input type="radio" name="winner_team_id" value="{{ $fixture->team2->id }}" {{ old('winner_team_id') == $fixture->team2->id ? 'checked' : '' }} class="mr-2" />
-                {{ $fixture->team2->long_name }}
+            <label class="flex justify-between items-center text-white p-4 border-b border-white">
+                <div>
+                    <input type="radio" name="winner_team_id" value="{{ $fixture->team2->id }}" {{ old('winner_team_id') == $fixture->team2->id ? 'checked' : '' }} class="mr-2" />
+                    {{ $fixture->team2->long_name }}
+                </div>
+                <div class="font-mono text-white-800">
+                    {{ $fixture->team2->country_code }}
+                    <span class="fi fi-{{ strtolower($fixture->team2->country_code) }}"></span>
+                </div>
             </label>
-
-            <label class="flex items-center text-white">
+            <label class="flex items-center text-white p-4">
                 <input type="radio" name="winner_team_id" value="" {{ old('winner_team_id') == '' ? 'checked' : '' }} class="mr-2" />
                 Draw/Tie
             </label>
-        </fieldset>
+        </div>
         @error('winner_team_id')
             <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
         @enderror
 
         <div class="flex justify-center items-center">
-            <button type="submit" class="mx-auto mt-4 px-12 py-4 bg-blue-500 text-white rounded-full">Bet</button>
+            <button type="submit" class="mx-auto mt-12 px-12 py-4 bg-blue-500 text-white rounded-full">Bet</button>
         </div>
     </form>
 @endsection
