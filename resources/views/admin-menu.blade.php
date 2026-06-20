387cd1cecd88af9e550da0e354cd709b05b773ea
Marin <marinmema@live.ca>
2026-06-19 22:57:16 -0400

    link back to feed

--- resources/views/admin-menu.blade.php
+++ resources/views/admin-menu.blade.php
@@ -3,7 +3,10 @@
 @section('title', config('app.name'))
 
 @section('content')
-    <h1 class="text-white font-bold my-4">Admin menu</h1>
+    <div class="flex justify-start items-center">
+        <a href="{{ route('feed') }}" class="text-gray-400 mb-4">Feed</a>
+    </div>
+    <h1 class="text-white font-bold mb-4">Admin menu</h1>
     <div class="border border-white rounded flex flex-col justify-start items-start">
         <a href="{{ route('bets.index') }}" class="w-full text-white inline-block border-b border-white p-4">Bets</a>
         <a href="{{ route('fixtures.index') }}" class="w-full text-white inline-block border-b border-white p-4">Fixtures</a>
