c81718852cba2aaba5fa52248e7ff139bc74fc2a
Marin <marinmema@live.ca>
2026-06-19 22:55:10 -0400

    add admin menu link

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -20,6 +20,14 @@
         </a>
     </div>
 
+    @if ($user->is_admin)
+        <a href="{{ route('admin-menu') }}" class="mt-8 border border-white rounded text-white flex justify-between items-center p-4">
+            <span>Admin menu</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
+            </svg>
+        </a>
+    @endif
 
     <form action="{{ route('logout') }}" method="POST" class="flex justify-center mt-16">
         @csrf
