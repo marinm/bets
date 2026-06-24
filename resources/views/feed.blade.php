7d2e4115aa949cf6aef58bfbac2abd1811b59abc
Marin <marinmema@live.ca>
2026-06-24 16:31:44 -0400

    collapse leaderboard

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -12,7 +12,13 @@
         </a>
     </div>
 
-    <div class="w-full bg-red-900 rounded-lg text-white border border-red-800">
+    <details class="overflow-hidden w-full bg-red-900 rounded-lg text-white border border-red-800">
+        <summary class="p-4 list-none flex justify-between items-center">
+            <div>Leaderboard</div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+            </svg>
+        </summary>  
         @foreach ($leaderboard as $user)
             <div class="p-4 border-b border-red-800 last:border-b-0 flex justify-between items-center text-white">
                 <div>{{ $user->name }}</div>
@@ -24,7 +30,7 @@
                 </div>
             </div>
         @endforeach
-    </div>
+    </details>
 
     <div class="mt-4 w-full flex flex-col justify-start items-center gap-4">
         @foreach ($dailyFixtures as $fixtureGroup)
