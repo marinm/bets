395d7a4330f1b03c59ef38c1bdb56ffa4a3076b6
Marin <marinmema@live.ca>
2026-06-29 19:13:07 -0400

    game can be in progress for 3 hr

--- app/Models/Fixture.php
+++ app/Models/Fixture.php
@@ -79,6 +79,6 @@ class Fixture extends Model
     {
         return (! $this->is_finished) &&
             $this->started_at <= now() &&
-            $this->started_at->clone()->addHours(2)->addMinutes(30) >= now();
+            $this->started_at->clone()->addHours(3) >= now();
     }
 }
