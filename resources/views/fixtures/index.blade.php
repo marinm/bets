fbde5d91bb1b02ca35a3706f5156b61f0bd6d3a3
Marin <marinmema@live.ca>
2026-06-28 21:08:57 -0400

    compact fixtures index

--- resources/views/fixtures/index.blade.php
+++ resources/views/fixtures/index.blade.php
@@ -10,15 +10,16 @@
         <a href="{{ route('fixtures.create') }}" class="px-4 py-2 bg-blue-500 text-white rounded">Create</a>
     </div>
 
-    <div class="flex flex-col gap-4">
+    <div class="border border-gray-800 rounded-lg overflow-hidden text-white font-mono">
         @foreach ($fixtures as $fixture)
-            <a href="{{ route('fixtures.show', ['fixture' => $fixture]) }}" class="bg-gray-800 p-4 rounded flex justify-between items-center">
-                <div class="flex-1">
-                    <p class="text-white text-lg font-bold truncate">{{ $fixture->team1->long_name }}</p>
-                    <p class="text-white text-lg font-bold truncate">{{ $fixture->team2->long_name }}</p>
-                    <p class="text-gray-400">{{ $fixture->started_at_local->format('Y-m-d H:i') }}</p>
+            <a href="{{ route('fixtures.show', ['fixture' => $fixture]) }}" class="border-b border-gray-800 p-4 rounded flex justify-between items-center">
+                <div class="w-full flex justify-between">
+                    <div>{{ $fixture->started_at_local->format('M d h:i A') }}</div>
+                    <div>
+                        {{ $fixture->team1->country_code }} -
+                        {{ $fixture->team2->country_code }}
+                    </div>
                 </div>
-                <p class="text-gray-400">{{ $fixture->is_finished ? 'Finished' : 'Upcoming' }}</p>
             </a>
         @endforeach
     </div>
